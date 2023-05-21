9e0bb2f77374b849c7e0941f5725a65f8bb20cd6
seikacu <neohack@xakep.ru>
2023-05-21 22:53:43 +0300

    initial cimmit

---
+++ operations.py
@@ -0,0 +1,25 @@
+# # ==
+# # a == b
+
+# # is
+# # a is b
+
+# list_1 = [1, 2, 3]
+# list_2 = [1, 2 ,3]
+
+# print(id(list_1))
+# print(id(list_2))
+
+# # print(list_1 == list_2)
+
+# print(list_1 is list_2)
+
+
+my_str_1 = 'Hello Friend'
+my_str_2 = 'Hello Friend'
+
+print(id(my_str_1))
+print(id(my_str_2))
+
+print(my_str_1 == my_str_2)
+print(my_str_1 is my_str_2)
